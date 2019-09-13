35826c231071996ed75b629cd12ff76dfe8f80e4
ak052142 <anveshkumar.kolluri@cerner.com>
2019-09-13 17:40:12 -0500

    Wraps the container with a column and adds a new column for the space to add todos

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,31 +5,58 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
       body: Container(
-        padding:
-            EdgeInsets.only(top: 60.0, left: 30.0, bottom: 30.0, right: 30.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            CircleAvatar(
-              radius: 30.0,
-              backgroundColor: Colors.white,
-              child: Icon(
-                Icons.list,
-                size: 30.0,
-                color: Colors.lightBlueAccent,
+            Container(
+              padding: EdgeInsets.only(
+                  top: 60.0, left: 30.0, bottom: 30.0, right: 30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 30.0,
+                    backgroundColor: Colors.white,
+                    child: Icon(
+                      Icons.list,
+                      size: 30.0,
+                      color: Colors.lightBlueAccent,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    'Todoey',
+                    style: TextStyle(
+                        fontSize: 50.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  Text(
+                    '12 Tasks',
+                    style: TextStyle(color: Colors.white, fontSize: 18.0),
+                  ),
+                ],
               ),
             ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'Todoey',
-              style: TextStyle(
-                  fontSize: 50.0,
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
                   color: Colors.white,
-                  fontWeight: FontWeight.w700),
-            ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
