b44d90226c6b219dced9dd4eda4d1a7d46e92a84
ak052142 <anveshkumar.kolluri@cerner.com>
2019-09-13 18:18:13 -0500

    Adds bottom sheet modal

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:todo_flutter/widgets/tasks_list.dart';
+import 'package:todo_flutter/widgets/tasks_tile.dart';
 
 class TasksScreen extends StatelessWidget {
+  Widget buildBottomSheet(BuildContext context) {
+    return Container();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,7 +14,9 @@ class TasksScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.lightBlueAccent,
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(context: context, builder: buildBottomSheet);
+        },
       ),
       body: Container(
         child: Column(
