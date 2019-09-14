262012741de7d185ae3fd31fd44b9428eaa8606b
ak052142 <anveshkumar.kolluri@cerner.com>
2019-09-13 20:15:21 -0500

    updating the state based on the checkbox

--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void checkBoxCallBack(bool newValue) {
+    setState(() {
+      isChecked = newValue;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Buy Milk'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: null,
-        checkColor: Colors.black,
-        activeColor: Colors.black,
+      title: Text(
+        'This is a task',
+        style: TextStyle(
+            decoration: isChecked == true ? TextDecoration.lineThrough : null),
+      ),
+      trailing: TaskCheckBox(
+        checkBoxState: isChecked,
+        toggleCheckboxState: checkBoxCallBack,
       ),
     );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function toggleCheckboxState;
+
+  TaskCheckBox({this.checkBoxState, this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(value: checkBoxState, onChanged: toggleCheckboxState);
   }
 }
